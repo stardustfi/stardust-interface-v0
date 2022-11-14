81f4d5c7381da714ce9a56f80d48a29752efe41a
0xYves <by0xyves@gmail.com>
2022-11-14 01:08:30 -0800

    fix next commits

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fe82da229af804a"; }
+/******/ 		__webpack_require__.h = function() { return "191a58c6cf4a7975"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
